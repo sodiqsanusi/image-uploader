7c578dfb4f124f9fbdce1a85c0e60a45e45a8e33
Sodiq Sanusi <ademilddiq@gmail.com>
2023-05-13 11:40:32 +0100

    Starting out with the success page. Used a placeholder image to demo things

--- frontend/pages/success.jsx
+++ frontend/pages/success.jsx
@@ -1,9 +1,31 @@
+import { MdCheckCircle, MdContentCopy } from "react-icons/md";
+import styles from "../styles/success.module.css";
+import testImage from "../public/images/FSiE4wdXEAAGkRb.jpeg";
+import Image from "next/image";
+import Head from "next/head";
+
 const Success = () => {
-  return ( 
+  return (
     <>
-      This is the success page!
+      <Head>
+        <title>Image Upload was successful!</title>
+      </Head>
+      <main className={styles.container}>
+        <MdCheckCircle />
+        <h1>Uploaded Successfully!</h1>
+        <div className={styles.imgContainer}>
+          <Image
+            src={testImage} priority
+            alt="The image uploaded from your device"
+          />
+        </div>
+        <div className={styles.copyLinkContainer}>
+          <p>https://images.yourdomain.com/8309i3090u2ifjoj09130jodjej0p9</p>
+          <button> <MdContentCopy /> </button>
+        </div>
+      </main>
     </>
   );
 }
- 
+
 export default Success;
